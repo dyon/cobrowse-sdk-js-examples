ffd74616a949c07ae76d0dd9dcc4d0de58030dbe
Dani <hello@dyon.me>
2024-04-19 14:24:02 +0200

    fix: encode external transaction's query params

--- web-example/src/components/ExternalTransactionView.jsx
+++ web-example/src/components/ExternalTransactionView.jsx
@@ -6,7 +6,7 @@ const ExternalTransactionView = ({ title, date, amount, category }) => (
   <iframe
     className={styles.frame}
     title='frame'
-    src={`${BASE_URL}?title=${title}&subtitle=${date}&amount=${amount}&category=${category}`}
+    src={`${BASE_URL}?title=${encodeURIComponent(title)}&subtitle=${encodeURIComponent(date)}&amount=${encodeURIComponent(amount)}&category=${encodeURIComponent(category)}`}
     allowFullScreen={true}
   ></iframe>
 )
