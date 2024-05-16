ebc3b6b5e17a28a91e4eaaf8d80429cd06135b59
Dani <hello@dyon.me>
2024-05-16 10:42:30 +0200

    chore: host external HTML views

--- web-example/src/components/ExternalTransactionView.jsx
+++ web-example/src/components/ExternalTransactionView.jsx
@@ -1,6 +1,6 @@
 import styles from './ExternalTransactionView.module.css'
 
-const BASE_URL = 'https://cobrowseio.github.io/cobrowse-sdk-ios-examples/'
+const BASE_URL = '/embeds/transaction.html'
 
 const ExternalTransactionView = ({ title, date, amount, category }) => (
   <iframe
