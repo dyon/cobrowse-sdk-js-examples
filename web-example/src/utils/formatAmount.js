e00181fc2ae20b5767afddd290759a8aa00744ab
Dani <hello@dyon.me>
2024-06-19 21:00:51 +0200

    fix: set default currency if it can't be determined with the language

--- web-example/src/utils/formatAmount.js
+++ web-example/src/utils/formatAmount.js
@@ -3,9 +3,11 @@ import LocaleCurrency from 'locale-currency'
 export const formatAmount = (amount) => {
   // If for some reason the language is not available, default to en-US
   const language = navigator.language ?? 'en-US'
+  // If the currency for the language can't be found, default to USD
+  const currency = LocaleCurrency.getCurrency(language) ?? 'USD'
 
   return amount.toLocaleString(language, {
     style: 'currency',
-    currency: LocaleCurrency.getCurrency(language)
+    currency
   })
 }
